8b0fe8c4e4c764046f3513b9cc0c8cdb6d12be91
AIKAWA Shigechika <shige@aikawa.jp>
2026-08-11 16:57:13 +0900

    chore(main): release 1.2.1

--- eos_mcp/__init__.py
+++ eos_mcp/__init__.py
@@ -5,4 +5,4 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("eos-mcp")
 except PackageNotFoundError:
-    __version__ = "1.2.0"
+    __version__ = "1.2.1"
